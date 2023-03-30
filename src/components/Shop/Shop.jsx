be1bd432588d1aaf3f5c1d78413f9a7438182031
najmussakib2 <nnsnajmussakib@gmail.com>
2023-03-30 18:08:39 +0600

    1st commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../product/product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setproducts] = useState([]);
+    const[cart, setCart] = useState([])
+
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setproducts(data));
+    },[])
+
+    const handleAddToCart = (product) => {
+       const newCart = [...cart, product];
+       setCart(newCart);
+    }
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                {
+                    products.map(product => <Product 
+                                key={product.id} 
+                                product={product}
+                                handleAddToCart= {handleAddToCart}></Product>)
+                }
+
+            </div>
+            <div className='card-container'>
+                <h2>Order summary</h2>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
---
+++ src/components/product/product.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    const handleAddToCart = props.handleAddToCart;
+    
+    const {img,name,seller,ratings,price} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price : ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} stars</p>
+            </div>
+            <button onClick={()=> handleAddToCart(props.product)} className='btn-cart'>Add to Cart<FontAwesomeIcon icon={faShoppingCart} /></button>
+        </div>
+    );
+};
+
+export default Product;
